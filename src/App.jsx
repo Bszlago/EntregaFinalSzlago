4887390750fda169afa96cb4488db42547bd01d6
Brandon Szlago <brandon.szlago@hotmail.com>
2023-07-20 14:09:48 -0300

    EntregaSzlago

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import NavBar from "./components/NavBar/NavBar"
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
-import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Productos from "./components/Productos/Productos"
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Cart from './components/Cart/Cart';
+import Checkout from './components/Checkout/Checkout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CarritoProvider } from './context/CarritoContext';
 
-const App = () => {
+function App() {
   return (
     <>
       <BrowserRouter>
-      <NavBar/>
-      <Routes>
-        <Route path='/' element={<ItemListContainer/>}/>
-        <Route path='/categoria/:idCategoria' element={<ItemListContainer/>}/>
-        <Route path='/item/:idItem' element={<ItemDetailContainer/>}/>
-      </Routes>
+        <CarritoProvider>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer />} />
+            <Route path='/categoria/:idCategoria' element={<ItemListContainer />} />
+            <Route path='/item/:idItem' element={<ItemDetailContainer />} />
+            <Route path='/cart' element={<Cart/>} />
+            <Route path='/checkout' element={<Checkout/>} />
+          </Routes>
+        </CarritoProvider>
       </BrowserRouter>
+
     </>
   );
 }
+
 
 export default App;
